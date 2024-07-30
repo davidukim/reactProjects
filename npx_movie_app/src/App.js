eb560f43f5293750c10387783fcd56188b1e774a
davidkim <can.davidkim@gmail.com>
2024-07-30 16:53:51 -0400

    useEffect --> when dep [__ , __] variables exist in the list, function will run on value changes

--- npx_movie_app/src/App.js
+++ npx_movie_app/src/App.js
@@ -6,20 +6,33 @@ import {useState, useEffect} from "react";
 
 function App() {
   let [count, setCount] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   console.log("i get called every re-render");
 
   const incrementor = () => {
     setCount(prev => prev + 1);
   }
 
-  const Call_API = () => {
-    console.log("i get called only once for initial render");
+  const ChangeKeyWord = (event) => {
+    setKeyword(event.target.value)
+    console.log(keyword);
   }
 
-  useEffect(Call_API, []);
+  useEffect( () => {
+    console.log("I run only once");
+  }, []);
+
+  useEffect( () => console.log("I run when keyword changes"), [keyword])
+  useEffect( () => console.log("I run when counter changes"), [count])
 
   return (
     <div>
+      <input 
+      type="text" 
+      placeholder="Search here.." 
+      value={keyword} 
+      onChange={ChangeKeyWord}></input>
       <h1>Our count: {count}</h1>
       <Button passedInProp={incrementor}/>
     </div>
