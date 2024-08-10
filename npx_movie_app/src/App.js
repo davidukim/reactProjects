d8cbb680aa0b3621467a27ba909510246f604cce
davidkim <can.davidkim@gmail.com>
2024-08-10 14:41:39 -0400

    start implementing todo

--- npx_movie_app/src/App.js
+++ npx_movie_app/src/App.js
@@ -5,38 +5,28 @@ import {useState, useEffect} from "react";
 
 
 function App() {
-  let [count, setCount] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  console.log("i get called every re-render");
-
-  const incrementor = () => {
-    setCount(prev => prev + 1);
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => {
+    setToDo(event.target.value);  
   }
-
-  const ChangeKeyWord = (event) => {
-    setKeyword(event.target.value)
-    console.log(keyword);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === ""){
+      return;
+    }
+    console.log(toDo);
+    setToDo("");
   }
-
-  useEffect( () => {
-    console.log("I run only once");
-  }, []);
-
-  useEffect( () => console.log("I run when keyword changes"), [keyword])
-  useEffect( () => console.log("I run when counter changes"), [count])
 
   return (
     <div>
-      <input 
-      type="text" 
-      placeholder="Search here.." 
-      value={keyword} 
-      onChange={ChangeKeyWord}></input>
-      <h1>Our count: {count}</h1>
-      <Button passedInProp={incrementor}/>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your todo..."/>
+        <button>Add To Do</button>
+      </form>
+      
     </div>
-  );
+  )
 }
 
 export default App;
