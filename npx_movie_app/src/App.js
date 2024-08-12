7b93e68e34e268978d1747a3f98a3fe31a37928c
davidkim <can.davidkim@gmail.com>
2024-08-11 22:53:04 -0400

    Todo list, using iterative li generation with map(elem,index,arr)

--- npx_movie_app/src/App.js
+++ npx_movie_app/src/App.js
@@ -6,6 +6,7 @@ import {useState, useEffect} from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => {
     setToDo(event.target.value);  
   }
@@ -14,19 +15,32 @@ function App() {
     if (toDo === ""){
       return;
     }
-    console.log(toDo);
+    setToDos((currentToDos) => [toDo, ...toDos]);
     setToDo("");
   }
-
+  useEffect(()=>{console.log(toDos)}, [toDos]);
   return (
     <div>
+      <h1>My To Do's ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} type="text" placeholder="Write your todo..."/>
         <button>Add To Do</button>
       </form>
-      
+      <hr/>
+      <ul>
+        {toDos.map((elem, index) => <li key={index}>{elem}</li>)}
+      </ul>
+
     </div>
   )
 }
+
+// javascript map parameters are in order of elem, index, array :: map(elem, index, array) -> naming to readability
+// Case 1: Only need access to elem :: map(elem)
+// Case 2: Only need access to index:: map(_, index)
+// Case 3: Only need access to array:: map(_, _, arr)
+// Case 4: Need access to elem and arr:: map(elem, _, arr)
+// Case 5: Need access to elem and index:: map(elem, index)
+// Case 6: Need access to index and arr:: map(index, _, arr) 
 
 export default App;
