873c51aa6826f3699f70dc02cf9d4ae9a0503948
davidkim <can.davidkim@gmail.com>
2024-08-14 22:21:34 -0400

    Coin Price Tracker with API fetch

--- npx_movie_app/src/App.js
+++ npx_movie_app/src/App.js
@@ -5,42 +5,25 @@ import {useState, useEffect} from "react";
 
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);  
-  }
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === ""){
-      return;
-    }
-    setToDos((currentToDos) => [toDo, ...toDos]);
-    setToDo("");
-  }
-  useEffect(()=>{console.log(toDos)}, [toDos]);
-  return (
-    <div>
-      <h1>My To Do's ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write your todo..."/>
-        <button>Add To Do</button>
-      </form>
-      <hr/>
-      <ul>
-        {toDos.map((elem, index) => <li key={index}>{elem}</li>)}
-      </ul>
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
-    </div>
-  )
+  useEffect(()=>{fetch("https://api.coinpaprika.com/v1/tickers").then( (response) => response.json()).then( json => {setCoins(json); setLoading(false);})}, [])
+  console.log(coins);
+
+  return (<div>
+    <h1>Coin Prices</h1>
+    <hr/>
+    {loading ? <strong>I am loading ...</strong> : <select>
+      {
+      coins.map((coin) => <option id={coin.id}>{coin.name}({coin.symbol}::Rank{coin.rank}) ${coin.quotes.USD.price.toFixed(2)} USD</option>)
+      }
+    </select>}
+
+    
+  </div>
+)
 }
 
-// javascript map parameters are in order of elem, index, array :: map(elem, index, array) -> naming to readability
-// Case 1: Only need access to elem :: map(elem)
-// Case 2: Only need access to index:: map(_, index)
-// Case 3: Only need access to array:: map(_, _, arr)
-// Case 4: Need access to elem and arr:: map(elem, _, arr)
-// Case 5: Need access to elem and index:: map(elem, index)
-// Case 6: Need access to index and arr:: map(index, _, arr) 
 
 export default App;
